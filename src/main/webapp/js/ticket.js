e15d5f7e7d0878b257eb423edcb1663c4625ffd9
Izabela Łach <ilach@opi.org.pl>
2021-05-03 00:01:05 +0200

    'front_biletow_poprawiony_jest_do_uporzadkowania_po_dodaniu_backendu'

--- src/main/webapp/js/ticket.js
+++ src/main/webapp/js/ticket.js
@@ -1,18 +1,64 @@
 $(document).ready(function () {
     setStyleInputForNumbers();
+    //changeTextButton();
+    //resetInput();
+    hiddenBtn();
+    hiddenForm();
 })
 
 function setStyleInputForNumbers() {
     $("input[type='number']").inputSpinner();
 }
 
-/*function setStyleInputForNumbers() {
-    $("input[type='number']").inputSpinner({
-        decrementButton: "<strong>-</strong>",
-        incrementButton: "<strong>+</strong>",
-        groupClass: "input-group-spinner",
-        buttonsClass: "btn-outline-secondary",
-        buttonsWidth: "2.5em",
-        textAlign: "center"
+function changeTextButton() {
+    $('.btn-sm').click(function () {
+        $(this).text(function (i,old) {
+            return old == "Dodaj kolejny" ?  'Schowaj kolejny' : 'Dodaj kolejny';
+        })
     })
-}*/
+}
+
+function resetInput() {
+    if($('#btnSmall1').click(function () {
+        $('#inpNo2').val("");
+    }));
+    if($('#btnSmall2').click(function () {
+        $('#inpNo3').val("");
+    }));
+    if($('#btnSmall3').click(function () {
+        $('#inpNo4').val("");
+    }));
+}
+
+function hiddenBtn() {
+    if($('#btnSmall1').click(function () {
+        $('#btnSmall1').hide();
+        $('#inpNo2').val("");
+    }));
+    if($('#btnSmall2').click(function () {
+        $('#btnSmall2').hide();
+        $('#btnSmall2del').show();
+        $('#inpNo3').val("");
+    }));
+    if($('#btnSmall3').click(function () {
+        $('#btnSmall3').hide();
+        $('#btnSmall3del').show();
+        $('#btnSmall4del').show();
+        $('#inpNo4').val("");
+    }));
+}
+
+function hiddenForm() {
+    if($('#btnSmall2del').click(function () {
+        $('#form2').hide();
+        $('#inpNo2').val("");
+    }));
+    if($('#btnSmall3del').click(function () {
+        $('#form3').hide();
+        $('#inpNo3').val("");
+    }));
+    if($('#btnSmall4del').click(function () {
+        $('#form4').hide();
+        $('#inpNo4').val("");
+    }));
+}
