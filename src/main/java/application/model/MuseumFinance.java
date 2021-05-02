90d5d85f4fabee40674cb3b2004fc7e1236ee590
Izabela Łach <ilach@opi.org.pl>
2021-05-02 13:34:21 +0200

    'poprawiony_panelAdmina_aby_nie_nachodzil_na_stopke'

--- src/main/java/application/model/MuseumFinance.java
+++ src/main/java/application/model/MuseumFinance.java
@@ -4,19 +4,31 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
-@Table(name="MUSEUM_FINANCE")
-@Getter
-@Setter
+@Table(name="MUSEUM_FINANCE", uniqueConstraints = {@UniqueConstraint(columnNames = "key")})
 @NoArgsConstructor
 public class MuseumFinance {
 
-    @Id
-    private int id = 1;
-
+    private String key;
     private double amount;
+
+    @Id
+    @Column(name = "key", unique = true, nullable = false)
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
 }
