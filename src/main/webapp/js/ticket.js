8d2d5bd89d8d617f5de0d2a8b70b1d993dd21f8d
Izabela Łach <ilach@opi.org.pl>
2021-05-11 13:47:55 +0200

    'poprawiony_front_biletow_dodac_liczenie_CENY_biletow'

--- src/main/webapp/js/ticket.js
+++ src/main/webapp/js/ticket.js
@@ -1,64 +1,69 @@
 $(document).ready(function () {
     setStyleInputForNumbers();
-    //changeTextButton();
-    //resetInput();
-    hiddenBtn();
-    hiddenForm();
+    getElement()
+    hideForms();
+    deleteForm();
+    setSum();
 })
+
+var inp1, inp2, inp3, inp4, result;
+
+function getElement() {
+    inp1 = document.getElementById('inpNo1');
+    inp2 = document.getElementById('inpNo2');
+    inp3 = document.getElementById('inpNo3');
+    inp4 = document.getElementById('inpNo4');
+    result = document.getElementById('inputSumPLN');
+}
 
 function setStyleInputForNumbers() {
     $("input[type='number']").inputSpinner();
 }
 
-function changeTextButton() {
-    $('.btn-sm').click(function () {
-        $(this).text(function (i,old) {
-            return old == "Dodaj kolejny" ?  'Schowaj kolejny' : 'Dodaj kolejny';
-        })
+function hideForms() {
+    $('#form2').hide();
+    $('#form3').hide();
+    $('#form4').hide();
+}
+
+function addForm() {
+    var form2 = document.getElementById('form2');
+    var isVisibleForm2 = form2.style.display.includes('none')
+    var form3 = document.getElementById('form3');
+    var isVisibleForm3 = form3.style.display.includes('none')
+    if(isVisibleForm2){
+        $('#form2').show();
+    }
+    else {
+        if(isVisibleForm3){
+            $('#form3').show();
+        }
+        else {
+            $('#form4').show();
+        }
+    }
+}
+
+function deleteForm() {
+    $('.deleteBtn').on('click', function(){
+        var form_id = $(this).parent().parent().attr('id')
+        var input_id = $(this).parent().prev().children().attr('id')
+        $('#'+input_id).val('0')
+        $('#'+form_id).hide()
+        updateSum()
     })
 }
 
-function resetInput() {
-    if($('#btnSmall1').click(function () {
-        $('#inpNo2').val("");
-    }));
-    if($('#btnSmall2').click(function () {
-        $('#inpNo3').val("");
-    }));
-    if($('#btnSmall3').click(function () {
-        $('#inpNo4').val("");
-    }));
+function setSum() {
+    inp1.onchange = showText;
+    inp2.onchange = showText;
+    inp3.onchange = showText;
+    inp4.onchange = showText;
+    function showText() {
+        result.value = parseFloat(inp1.value) + parseFloat(inp2.value) + parseFloat(inp3.value) + parseFloat(inp4.value);
+    }
 }
 
-function hiddenBtn() {
-    if($('#btnSmall1').click(function () {
-        $('#btnSmall1').hide();
-        $('#inpNo2').val("");
-    }));
-    if($('#btnSmall2').click(function () {
-        $('#btnSmall2').hide();
-        $('#btnSmall2del').show();
-        $('#inpNo3').val("");
-    }));
-    if($('#btnSmall3').click(function () {
-        $('#btnSmall3').hide();
-        $('#btnSmall3del').show();
-        $('#btnSmall4del').show();
-        $('#inpNo4').val("");
-    }));
-}
-
-function hiddenForm() {
-    if($('#btnSmall2del').click(function () {
-        $('#form2').hide();
-        $('#inpNo2').val("");
-    }));
-    if($('#btnSmall3del').click(function () {
-        $('#form3').hide();
-        $('#inpNo3').val("");
-    }));
-    if($('#btnSmall4del').click(function () {
-        $('#form4').hide();
-        $('#inpNo4').val("");
-    }));
+function updateSum() {
+    result.value = parseFloat(inp1.value) + parseFloat(inp2.value) + parseFloat(inp3.value) + parseFloat(inp4.value);
 }
