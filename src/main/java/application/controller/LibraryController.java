bcd60f079288961140b2f8243e744cbeddc58cc3
Izabela Łach <ilach@opi.org.pl>
2021-07-22 16:47:01 +0200

    'swagger_zostal_podlaczony'

--- src/main/java/application/controller/LibraryController.java
+++ src/main/java/application/controller/LibraryController.java
@@ -2,8 +2,11 @@ package application.controller;
 
 import application.dto.BooksDTO;
 import application.dto.DonationStatisticDTO;
+import application.model.Book;
 import application.service.DonationService;
 import application.service.LibraryService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Api(value = "MUSEUM book API controller")
 @RestController
 public class LibraryController {
 
@@ -20,6 +24,7 @@ public class LibraryController {
         this.libraryService = libraryService;
     }
 
+    @ApiOperation(value = "Get all books", response = Book.class)
     @GetMapping(value = "/getBooks")
     public List<BooksDTO> getAllBooks(){
         return libraryService.getBooks();
